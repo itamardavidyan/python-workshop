3633b8ace027206b0008ff9c3f91e09e3f97d0b7
Itamar Davidyan <itamardavidyan@gmail.com>
2018-12-21 10:25:05 +0200

    first commit

---
+++ ex-1.py
@@ -0,0 +1,78 @@
+# commets with '#'
+
+# Data Types:
+    # - Integers --> 1 , 100, -1 , 0, 999 ...
+    # - Floating-Point Numbers --> 1.0 , 1.9 , 22.9 ...
+    # - Strings --> 'Hello World' , "double" , "can include numbers 123"
+    # - Boolean --> True , False
+
+# Built-In Functions:
+    # - print() Print in the console.
+    # - range() Generates a range of integer values.
+    # - abs()	Returns absolute value of a number.
+    # - max() Returns the largest of the given arguments or items in an iterable.
+    # - min()	Returns the smallest of the given arguments or items in an iterable.
+    # - pow()	Raises a number to a power.
+    # - round() Rounds a floating-point value.
+    # - sum()	Sums the items of an iterable.
+    # - int()	Returns an integer object constructed from a number or string.
+    # - str()	Returns a string version of an object.
+    # - len()	Returns the length of an object.
+
+#Ex:
+
+# 1-1:
+# first_name = 'Itamar'
+# last_name = 'Davidyan'
+
+# print(first_name + ' ' + last_name)
+# print(len(first_name + ' ' + last_name))
+
+# 1-2:
+# age = 23
+# print(age)
+# print(age + 1)
+# print(age)
+# print(str(age) + '1')
+
+# 1-3:
+# s_age = '23'
+# print(s_age)
+# print(s_age + '1')
+# print(int(s_age) + 1)
+
+# 1-3:
+# f = 7.0
+# f = f + 1.7
+# print(abs(f))
+# print(round(f))
+# print(round(f - 0.2))
+# print(round(f - 0.200000001))
+
+# 1-4:
+# print(pow(4,3))
+# print(pow(625,0.5))
+
+# 1-5:
+# bt = True
+# bf = False
+# print(bt)
+# print(bf)
+
+# 1-6:
+# print(range(10))
+# s = sum(range(10))
+# print(s)
+# ??? oneline ???
+
+# 1-7:
+# print(range(2, 15))
+# print(range(2, 15, 2))
+# print(range(2, 15, 3))
+# print(range(0, 15))
+
+# 1-8:
+# min = min(range(6, 9))
+# max = max(range(6, 9))
+# print('min = ' + str(min) + ' max = ' + str(max))
+
